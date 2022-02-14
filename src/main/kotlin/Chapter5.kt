02fa0d755939869c5d5ad33878e720ad9be9c4c9
Min Kee Choi <mkc0615@gmail.com>
2022-02-14 11:44:50 +0900

    Chapter5 workout codes

--- src/main/kotlin/Chapter5.kt
+++ src/main/kotlin/Chapter5.kt
@@ -16,6 +16,28 @@ fun findTheOldest(people : List<PersonCls>){
     println(theOldest)
 }
 
+val names = people.joinToString ( separator = " ", transform={p:PersonCls -> p.name} )
+
+fun printMessagesWithPrefix(messages: Collection<String>, prefix: String) {
+    messages.forEach {
+        println("$prefix $it")
+    }
+}
+
+val canBeInClub30 = { p:PersonCls -> p.age <= 30 }
+
+val peopleList = listOf(PersonCls("Alice", 29), PersonCls("Bob", 31), PersonCls("Carol", 31))
+
+
+val numList = listOf(1,2,3,4)
+
+val resultList = numList.asSequence()
+.map{ print("map($it) "); it * it}
+.filter{ print("filter($it) "); it % 2 == 0 }
+// .toList()
+
+val errors = listOf("403 Forbidden", "404 Not Found")
+
 class Chapter5 {
 
     fun workout(){
@@ -25,6 +47,10 @@ class Chapter5 {
         println(people.maxByOrNull{ it.age })
 
         println(people.maxByOrNull(PersonCls::age))
+
+        printMessagesWithPrefix(errors, "Error: ")
+
+        println(resultList)
 
     }
 }
